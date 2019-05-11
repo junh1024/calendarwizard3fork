cd61795cb31ce8692e361319ae16e6ba500c2336
junh1024 <junh1024@gmail.com>
2019-05-11 18:16:05 +1200

    code cleanup

--- InsertPicsFromText.js
+++ InsertPicsFromText.js
@@ -1,15 +1,5 @@
-
-
-
-// var fs = require("fs");
 var file1 = File.openDialog("select text file");
 file1.open("r");
-
-
-
-
-
-// alert(file1.read().split("\r\n"));
 
 mypath=file1.readln () ; //1st line is path
 lines=file1.read().split('\n');
@@ -19,63 +9,44 @@ var mm = 2.83464567; // Metric MM converter…
 // Set the script to work with artboard rulers	
 app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;	
 
+var firstImageLayer = true;
+var newLayer;
+var thisPlacedItem;
+var posX=0;
+var posY=0;
+var count=0;
 
+// var imageList = lines.getFiles();
 
-	
-	var firstImageLayer = true;
-	var newLayer;
-	var thisPlacedItem;
-	var posX=0;
-	var posY=0;
-	var count=0;
+for (var i = 0; i < lines.length; i++)
+{
+// if (imageList[i] instanceof File) {
+	var fileName = mypath+lines[i];
 
-	// create document list from files in selected folder
-	// var imageList = lines.getFiles();
-
-	for (var i = 0; i < lines.length; i++) {
-		// if (imageList[i] instanceof File) {
-			var fileName = mypath+lines[i];
-			// if( (fileName.indexOf(".svg") == -1) ) {
-				// continue;
-			// } else {
-				if( firstImageLayer ) {
-					newLayer = document.layers[0];
-					firstImageLayer = false;
-				} else {
-					newLayer = document.layers.add();
-				}
-				// Give the layer the name of the image file
-				newLayer.name = fileName.substring(0, fileName.indexOf(".") );
-	 
-				// Place the image on the artboard
-				var thisfile = File(fileName);
-				alert(fileName);
-				alert(thisfile);
-				// var thisfile = File("\~\Pictures\KC\AL Ships2\marker\AL Atago.png");
-				newGroup = newLayer.groupItems.createFromFile( thisfile );
-				newGroup.position = [ posX , posY ];
-			// }
-		// }
-		posX += newGroup.width;
-		if(posX > (newGroup.width*16)) {
-			posX = 0;
-			posY -= newGroup.height;
-		}
+	if( firstImageLayer ) 
+	{
+		newLayer = document.layers[0];
+		firstImageLayer = false;
+	} else 
+	{
+		newLayer = document.layers.add();
 	}
-	if( firstImageLayer ) {
-		// alert("The action has been cancelled.");
-		// display error message if no supported documents were found in the designated folder
-		alert("Sorry, but the designated folder does not contain any recognized image formats.\n\nPlease choose another folder.");
-		document.close();
-		importFolderAsLayers(getFolder());
-	}
- 
-// else {
-	// alert("The action has been cancelled.");
-	// display error message if no supported documents were found in the designated folder
-	// alert("Rerun the script and choose a folder with images.");
-	// importFolderAsLayers(getFolder());
+	// Give the layer the name of the image file
+	newLayer.name = lines[i].substring(0, fileName.indexOf(".") );
+
+	// Place the image on the artboard
+	var thisfile = File(fileName);
+	// alert(fileName);
+	// alert(thisfile);
+	// var thisfile = File("\~\Pictures\KC\AL Ships2\marker\AL Atago.png");
+	newGroup = newLayer.groupItems.createFromFile( thisfile );
+	newGroup.position = [ posX , posY ];
+	// }
 // }
-
-
-
+	posX += newGroup.width;
+	if(posX > (newGroup.width*16)) 
+	{
+		posX = 0;
+		posY -= newGroup.height;
+	}
+}
--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
+Adobe Creative Suite Scripts
+===
+
+CalendarWizard3 Fork
+---
 These changes to CW3 are optimized for my use. Some stuff is deleted.
 
 It's optimized for a feature-rich calendar in a very small space, with a lot of hardcoded/changed options/defaults, so this is probably not for you, but it may serve as a practical API in-use example.
@@ -5,3 +10,8 @@ It's optimized for a feature-rich calendar in a very small space, with a lot of 
 Original was GPL so this is too. https://sourceforge.net/projects/calendarwizard/
 
 See http://calendarwizard.sourceforge.net/
+
+InsertPicsFromText
+---
+Modified from importSVGFilesAsLayers.js
+https://gist.github.com/robmoggach/9df50a1e4eb936a5a2db
